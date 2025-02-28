84e7265cc52fc16c0aa33b47e429e150e1809a64
uw819kt <miya0arima@gmail.com>
2025-03-01 00:21:40 +0900

    feat: 酒気帯び検索機能追加

--- app/models/alcohol_log.rb
+++ app/models/alcohol_log.rb
@@ -13,4 +13,12 @@ class AlcoholLog < ApplicationRecord
   enum :driving_status, {before: 0, after: 1}
   enum :confirmation, {phone: 0, meeting: 1, others1: 2}
   enum :condition, {good: 0, bad: 1, others2: 2}
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["car_id", "check_time", "condition", "confirmation", "created_at", "detector_used", "driving_status", "id", "log_remarks", "result", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user", "car"]
+  end
 end
